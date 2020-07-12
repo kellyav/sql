e011a4dfc3dbe8ba2355351eee3a62064d935c2e
Alexa Kelly <kellyav@dukes.jmu.edu>
2020-07-12 17:32:21 -0400

    Create growth_pop.sql

---
+++ growth_pop.sql
@@ -0,0 +1,44 @@
+--determine the percentage increase from one year to the next:
+SELECT p1.country_code,
+       p1.size AS size2010, 
+       p2.size AS size2015,
+-- calculate growth_perc, 
+       ((p2.size - p1.size)/p1.size * 100.0) AS growth_perc
+FROM populations AS p1
+-- Join to itself 
+  INNER JOIN populations AS p2
+-- Match on country code
+    ON p1.country_code = p2.country_code
+        -- and year (with calculation)
+        AND p1.year = p2.year - 5;
+-- note: growth_perc calculates the percentage population growth from 2010 to 2015 for each country
+
+
+--which countries had high average life expectancies (at the country level) in 2015.
+SELECT *
+  FROM populations
+WHERE life_expectancy >
+  -- 1.15 * subquery
+  1.15 * (SELECT AVG(life_expectancy)
+   		FROM populations
+   		WHERE year = 2015) AND
+  	year = 2015;
+    
+    
+--determining the top 10 capital cities in Europe and the Americas in terms of a calculated percentage using city_proper_pop and metroarea_pop in cities.
+SELECT 
+	name, 
+	country_code, 
+	city_proper_pop, 
+	metroarea_pop,  
+-- Calculate city_perc
+       city_proper_pop/ metroarea_pop * 100 AS city_perc
+FROM cities
+WHERE name IN
+    (SELECT capital
+     FROM countries
+     WHERE (continent = 'Europe'
+        OR continent LIKE '%America'))
+        AND metroarea_pop IS NOT NULL
+ORDER BY city_perc DESC
+LIMIT 10;
