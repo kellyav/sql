67d3a3cf198af033109671265289ddcfd77661db
Alexa Kelly <kellyav@dukes.jmu.edu>
2020-07-12 16:46:53 -0400

    Update avg_goals.sql

--- avg_goals.sql
+++ avg_goals.sql
@@ -1,6 +1,6 @@
 /* SQL
 
-How does each months total goals differ from the average monthly total goals scored? *\
+How does each months total goals differ from the average monthly total goals scored? */
 
 SELECT 
 	EXTRACT(MONTH FROM date) AS month,	
@@ -23,7 +23,7 @@ GROUP BY month;
 FROM match
 GROUP BY month)
 /* logic: first, select the sum of goals scored in each month. Month is taken from date.
-Returns: two columns in a table, first is month (1-12), second is goals. *\
+Returns: two columns in a table, first is month (1-12), second is goals. */
 
 
 -- Secondmost outer (scalar) subquery:
@@ -36,4 +36,4 @@ FROM (SELECT
 /* this section calculates an average of the values generated in the previous table.
 Returns: 2944.75, which is the average monthly goals scored. 
 
-The entire nested subquery returns a scalar value to compare to the SUM of goals scored in each month.*\
+The entire nested subquery returns a scalar value to compare to the SUM of goals scored in each month. */
