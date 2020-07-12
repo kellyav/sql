0e75833deaa3c2a22f5dead7ece05aaab2a68199
Alexa Kelly <kellyav@dukes.jmu.edu>
2020-07-12 17:38:50 -0400

    Update city_names.sql

--- city_names.sql
+++ city_names.sql
@@ -14,3 +14,20 @@ FROM countries
 SELECT name
 FROM cities
 ORDER BY capital;
+
+
+--Identify the country codes that are included in either economies or currencies but not in populations
+SELECT name, country_code
+ FROM  cities AS c1
+ WHERE country_code IN
+	(SELECT e.code
+   	 FROM economies AS e
+    -- Get all additional (unique) values of the field from currencies AS c2  
+    	UNION
+    	SELECT c2.code
+    	FROM currencies AS c2
+    -- Exclude those appearing in populations AS p
+    	EXCEPT
+    	SELECT p.country_code
+    	FROM populations AS p
+	);
